929dd1d97dbb77323846111e3b26de03af4da3fd
LupinaIgor <igorlupina222@gmail.com>
2024-08-28 17:02:18 +0300

    Controllers

---
+++ WebApplicationGeo111/WebApplicationGeo111/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebApplicationGeo111.Models.Entities.Geo;
+
+namespace WebApplicationGeo111.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+   public DbSet<CountryModel> Countries { get; set; }
+   
+   public DbSet<AreaModel> Areas { get; set; }
+   
+   public DbSet<CityModel> Cities { get; set; }
+   
+   public DbSet<RegionModel> Regions { get; set; }
+   
+   
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
